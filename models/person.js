b014ce4a723c20c5ea5b4dc79d666dc81caea004
1mmanuel <99174608+1mmanuel@users.noreply.github.com>
2024-05-08 11:52:33 +0300

    addded custom validation for phone number

--- models/person.js
+++ models/person.js
@@ -23,7 +23,13 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    minLength: 3,
+    minLength: 8,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d{1,}$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
     required: true,
   },
 });
